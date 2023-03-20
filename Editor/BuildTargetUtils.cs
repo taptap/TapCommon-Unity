32b0034b7b070389ee6584aff6a61ce277273b73
ci-gitlab <tds-developer@xd.com>
2023-03-20 14:37:10 +0800

    feat:update upm

---
+++ Editor/BuildTargetUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace TapTap.Common.Editor {
+    public static class BuildTargetUtils {
+        public static bool IsSupportMobile(BuildTarget platform) {
+            return platform == BuildTarget.iOS || platform == BuildTarget.Android;
+        }
+
+        public static bool IsSupportStandalone(BuildTarget platform) {
+            return platform == BuildTarget.StandaloneOSX || platform == BuildTarget.StandaloneWindows;
+        }
+    }
+}
