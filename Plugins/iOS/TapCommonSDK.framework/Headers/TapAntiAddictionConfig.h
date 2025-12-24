e6cc306658ed40b6381ab1c9cf74cfc6a2bee596
ci-gitlab <tds-developer@xd.com>
2025-12-24 15:45:07 +0800

    feat:update upm

---
+++ Plugins/iOS/TapCommonSDK.framework/Headers/TapAntiAddictionConfig.h
@@ -0,0 +1,19 @@
+//
+//  TapAntiAddictionConfig.h
+//  TapCommonSDK
+//
+//  Created by 黄驿峰 on 2023/11/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TapAntiAddictionConfig : NSObject
+
+@property (nonatomic, assign) BOOL showSwitchAccount;
+@property (nonatomic) BOOL useAgeRange;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Runtime/Public/Event/EventConst.cs
@@ -0,0 +1,10 @@
+namespace TapTap.Common
+{
+    public sealed class EventConst
+    {
+        public const string SetRND = "SetRND";
+
+        public const string IsLaunchedFromTapTapPCFinished = "IsLaunchedFromTapTapPCFinished";
+    }
+
+}
